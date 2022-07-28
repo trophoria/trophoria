8ddc20ceba3c3c2874f97ec388dce530ff7a9b86
DevTobias <tobi.kaerst@gmx.de>
2022-07-28 21:00:33 +0200

    cd: added redis image to pipeline

--- backend/deploy/redis/init-redis.sh
+++ backend/deploy/redis/init-redis.sh
@@ -4,6 +4,4 @@ echo never > /sys/kernel/mm/transparent_hugepage/defrag
 sysctl -w net.core.somaxconn=512
 sysctl vm.overcommit_memory=1
 
-# redis-server /usr/local/etc/redis/redis.conf --loadmodule /usr/lib/redis/modules/rejson.so --requirepass $1
-# redis-server /usr/local/etc/redis/redis.conf --requirepass $1
 redis-server --requirepass $1
