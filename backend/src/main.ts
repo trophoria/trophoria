f90cb297c2d148e6199537db8932292a1507b5b3
Tobias Kärst <tobias.kaerst@qbeyond.de>
2022-08-08 01:12:27 +0200

    TROPH-11: Extracted app initialization to own method

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,23 +10,26 @@ import {
 import { AppModule } from '@trophoria/app.module';
 import { ThrottlerExceptionFilter } from '@trophoria/core/filters/throttler-exception.filter';
 
-const bootstrap = async () => {
-  const isProduction = process.env.NODE_ENV === 'production';
-  const apiPort = process.env.PORT ?? 3000;
-  const cookieSecret = process.env.COOKIE_SECRET;
-
+const bootstrapApp = async () => {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  await initializeApp(app);
+
+  app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+};
+
+const initializeApp = async (app: NestFastifyApplication) => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const cookieSecret = process.env.COOKIE_SECRET;
 
   await app.register(helmet, { contentSecurityPolicy: isProduction });
   await app.register(fastifyCookie, { secret: cookieSecret });
   await app.register(fastifyCsrf, { cookieOpts: { signed: true } });
 
   app.useGlobalFilters(new ThrottlerExceptionFilter());
-
-  app.listen(apiPort, '0.0.0.0');
 };
 
-bootstrap();
+bootstrapApp();
